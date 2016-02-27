4986a07402f2cf65f3c324b7fb8f47bbb4bbdf8b
ryanrfox <ryan@ryanrfox.com>
2016-02-27 22:10:05 +1100

    Add: install from PPA

--- bitshares-ubuntu-vm/build_bitshares.sh
+++ bitshares-ubuntu-vm/build_bitshares.sh
@@ -7,15 +7,22 @@ set -x
 date
 ps axjf
 
-################################################################
-# Build BitShares from source                                  #
-################################################################
+#################################################################
+# Update Ubuntu and install prerequisites for running BitShares #
+#################################################################
+time apt-get -y update
+time apt-get install -y bitshares dphys-swapfile ntp
+
+if [ $1 = 'Build' ]; then
+#################################################################
+# Build BitShares from source                                   #
+#################################################################
 NPROC=$(nproc)
 echo "nproc: $NPROC"
-################################################################
-# Update Ubuntu and install all necessary prerequisites        #
-################################################################
-#time apt-get -y update && apt-get -y install dphys-swapfile git ntp cmake libbz2-dev libdb++-dev libdb-dev libssl-dev openssl libreadline-dev autoconf libtool libboost-all-dev
+#################################################################
+# Install all necessary packages for building BitShares         #
+#################################################################
+#time apt-get && apt-get -y install git cmake libbz2-dev libdb++-dev libdb-dev libssl-dev openssl libreadline-dev autoconf libtool libboost-all-dev
 
 #cd /usr/local
 #time git clone https://github.com/bitshares/bitshares-2.git
@@ -28,23 +35,28 @@ echo "nproc: $NPROC"
 #chmod +x /etc/init.d/bitshares
 #update-rc.d bitshares defaults
 
-################################################################
-# Install BitShares from PPA                                   #
-################################################################
+else    
+#################################################################
+# Install BitShares from PPA                                    #
+#################################################################
 time add-apt-repository -y ppa:bitshares/bitshares
-time apt-get -y update && apt-get -y install dphys-swapfile ntp
+time apt-get -y update
 time apt-get install -y bitshares
+fi
 
+#################################################################
+# Configure BitShares witeness node to auto start at boot       #
+#################################################################
 printf '%s\n%s\n' '#!/bin/sh' '/usr/local/bitshares-2/programs/witness_node/witness_node --rpc-endpoint=127.0.0.1:8090' >> /etc/init.d/bitshares
 chmod +x /etc/init.d/bitshares
 update-rc.d bitshares defaults
 
-################################################################
-# BitShares installed. After reboot node will auto start       #
-################################################################
+#################################################################
+# BitShares installed. Reboot to start the witness node         #
+#################################################################
 reboot
 
-################################################################
-# Connect to the host via SSH, then start cli wallet           #
-# sudo /usr/local/bitshares-2/programs/cli_wallet/cli_wallet   #
-################################################################
+#################################################################
+# Connect to the host via SSH, then start cli wallet            #
+# sudo /usr/local/bitshares-2/programs/cli_wallet/cli_wallet    #
+#################################################################
