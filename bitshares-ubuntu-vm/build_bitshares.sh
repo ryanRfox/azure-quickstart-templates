2e7b1dd40f1e68fce5d6c95bfd18afc0e1534986
ryanrfox <ryan@ryanrfox.com>
2016-02-28 22:57:14 +1100

    Add: install from PPA

--- bitshares-ubuntu-vm/build_bitshares.sh
+++ bitshares-ubuntu-vm/build_bitshares.sh
@@ -11,7 +11,7 @@ ps axjf
 # Update Ubuntu and install prerequisites for running BitShares #
 #################################################################
 time apt-get -y update
-time apt-get install -y bitshares dphys-swapfile ntp
+time apt-get install -y dphys-swapfile ntp
 
 if [ $1 = 'From_Source' ]; then
 #################################################################
@@ -31,9 +31,6 @@ echo "nproc: $NPROC"
 #time cmake -DCMAKE_BUILD_TYPE=Release .
 #time make -j$NPROC
 #
-#printf '%s\n%s\n' '#!/bin/sh' '/usr/local/bitshares-2/programs/witness_node/witness_node --rpc-endpoint=127.0.0.1:8090' >> /etc/init.d/bitshares
-#chmod +x /etc/init.d/bitshares
-#update-rc.d bitshares defaults
 
 else    
 #################################################################
@@ -41,23 +38,24 @@ else
 #################################################################
 time add-apt-repository -y ppa:bitshares/bitshares
 time apt-get -y update
-time apt-get install -y bitshares
+time apt-get install -y bitshares2-cli
+
 fi
 
 #################################################################
 # Configure BitShares witeness node to auto start at boot       #
 #################################################################
-printf '%s\n%s\n' '#!/bin/sh' '/usr/local/bitshares-2/programs/witness_node/witness_node --rpc-endpoint=127.0.0.1:8090' >> /etc/init.d/bitshares
+printf '%s\n%s\n' '#!/bin/sh' '/usr/bin/witness_node --rpc-endpoint=127.0.0.1:8090 -d /usr/local/bitshares-2/programs/witness_node/'>> /etc/init.d/bitshares
 chmod +x /etc/init.d/bitshares
 update-rc.d bitshares defaults
 
 #################################################################
-# BitShares installed. Reboot to start the witness node         #
+# BitShares installed. Reboot host to start the witness node    #
 #################################################################
 reboot
 exit 0
 
 #################################################################
-# Connect to the host via SSH, then start cli wallet            #
+# Connect to host via SSH, then start cli wallet:               #
 # sudo /usr/local/bitshares-2/programs/cli_wallet/cli_wallet    #
 #################################################################
