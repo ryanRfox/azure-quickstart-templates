ac840095c9150891138e3eb98c8bb2f24e6ef312
ryanrfox <ryan@ryanrfox.com>
2016-02-29 15:50:40 +1100

    Add: install from PPA

--- bitshares-ubuntu-vm/README.md
+++ bitshares-ubuntu-vm/README.md
@@ -1,12 +1,7 @@
 # BitShares witness node and CLI wallet on Ubuntu 15.10 VM
 
-<<<<<<< HEAD
 <a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fryanrfox%2Fazure-quickstart-templates%2Freview%2Fbitshares-ubuntu-vm%2Fazuredeploy.json" target="_blank"><img src="http://azuredeploy.net/deploybutton.png"/></a>
 <a href="http://armviz.io/#/?load=https%3A%2F%2Fraw.githubusercontent.com%2Fryanrfox%2Fazure-quickstart-templates%2Freview%2Fbitshares-ubuntu-vm%2Fazuredeploy.json" target="_blank">
-=======
-<a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fryanrfox%2Fazure-quickstart-templates%2Fdev%2Fbitshares-ubuntu-vm%2Fazuredeploy.json" target="_blank"><img src="http://azuredeploy.net/deploybutton.png"/></a>
-<a href="http://armviz.io/#/?load=https%3A%2F%2Fraw.githubusercontent.com%2Fryanrfox%2Fazure-quickstart-templates%2Fdev%2Fbitshares-ubuntu-vm%2Fazuredeploy.json" target="_blank">
->>>>>>> dev
     <img src="http://armviz.io/visualizebutton.png"/>
 </a>
 
--- bitshares-ubuntu-vm/build_bitshares.sh
+++ bitshares-ubuntu-vm/build_bitshares.sh
@@ -24,35 +24,13 @@ echo "nproc: $NPROC"
 #################################################################
 time apt-get && apt-get -y install git cmake libbz2-dev libdb++-dev libdb-dev libssl-dev openssl libreadline-dev autoconf libtool libboost-all-dev
 
-<<<<<<< HEAD
-################################################################
-# Build BitShares from source, then set it start on boot       #
-################################################################
-=======
->>>>>>> dev
 cd /usr/local
 time git clone https://github.com/bitshares/bitshares-2.git
 cd bitshares-2/
 time git submodule update --init --recursive
 time cmake -DCMAKE_BUILD_TYPE=Release .
 time make -j$NPROC
-<<<<<<< HEAD
 
-printf '%s\n%s\n' '#!/bin/sh' '/usr/local/bitshares-2/programs/witness_node/witness_node --rpc-endpoint=127.0.0.1:8090' >> /etc/init.d/bitshares
-chmod +x /etc/init.d/bitshares
-update-rc.d bitshares defaults
-
-
-################################################################
-# BitShares is now built and installed, time to reboot         #
-################################################################
-reboot
-
-################################################################
-# Connect to the host via SSH, then start cli wallet           #
-# sudo /usr/local/bitshares-2/programs/cli_wallet/cli_wallet #
-################################################################
-=======
 cp /usr/local/bitshares-2/programs/witness_node/witness_node /usr/bin/witness_node
 cp /usr/local/bitshares-2/programs/cli_wallet/cli_wallet /usr/bin/cli_wallet
 
@@ -87,4 +65,3 @@ exit 0
 # $nano /usr/local/bitshares-2/programs/cli-wallet/wallet.json                                   #
 # Learn more: http://docs.bitshares.eu                                                           #
 ##################################################################################################
->>>>>>> dev
