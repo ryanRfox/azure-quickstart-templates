dadcd9992c0a34a31e848afa48220a6c8b3812b0
Fox <ryanRfox@users.noreply.github.com>
2016-06-22 13:25:02 +1000

    Update build_steemit.sh

--- steemit-ubuntu-vm/build_steemit.sh
+++ steemit-ubuntu-vm/build_steemit.sh
@@ -17,17 +17,19 @@ chmod +x ./jq
 mv jq /usr/bin
 curl -o /home/$USER_NAME/exists.json https://steemd.com/api/account/exists?name=$DESIRED_NAME
 AVAILABLE=$(cat exists.json | jq .'available')
-if [ $AVAILABLE = 'false' ]; then
+
+if $1 = ''; then
 cat >/home/$USER_NAME/available.var <<EOL
-then $AVAILABLE
+This is THEN 
+$AVAILABLE
 EOL
 exit 1
 
 else
 cat >/home/$USER_NAME/available.var <<EOL
-then $AVAILABLE
-EOL 
-
+This is ELSE
+$AVAILABLE
+EOL
 fi
 
 #################################################################
