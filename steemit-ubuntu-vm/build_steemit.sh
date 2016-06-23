9fe2925ff27bfc0d8535f21c2574592d435e10cc
Fox <ryanRfox@users.noreply.github.com>
2016-06-23 18:42:27 +1000

    Update build_steemit.sh

--- steemit-ubuntu-vm/build_steemit.sh
+++ steemit-ubuntu-vm/build_steemit.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-set -x
-date
-ps axjf
-
-NPROC=$(nproc)
-INSTALL_METHOD=$1
-USER_NAME=$2
-DESIRED_NAME=$3
-
-printf '%s\n%s\n' 'USER_NAME=$USER_NAME' 'DESIRED_NAME=$DESIRED_NAME' >> /home/$USER_NAME/steem_install.log
+printf '%s\n\t%s\n' 'first' 'second' >> file.txt
