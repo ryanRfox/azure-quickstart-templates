a89dd90912cf2032e3c782abe114b233442bd7f5
Fox <ryanRfox@users.noreply.github.com>
2016-06-24 15:54:26 +1000

    Update build_steemit.sh

--- steemit-ubuntu-vm/build_steemit.sh
+++ steemit-ubuntu-vm/build_steemit.sh
@@ -9,8 +9,7 @@ INSTALL_METHOD=$1
 USER_NAME=$2
 DESIRED_NAME=$3
 
-printf '%s\n%s\n' 'USER_NAME=$USER_NAME' 'DESIRED_NAME=$DESIRED_NAME'>> /home/$USER_NAME/steem_install.log
-
+printf '%s\n%s\n' 'USER_NAME='$USER_NAME 'DESIRED_NAME='$DESIRED_NAME >> /home/$USER_NAME/steem_install.log
 
 #################################################################
 # Verify desired name availability                              #
