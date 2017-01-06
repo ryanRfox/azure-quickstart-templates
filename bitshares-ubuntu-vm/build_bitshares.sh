a71878a517189bde1cdac26d44eb45e5a26ae793
Fox <ryanRfox@users.noreply.github.com>
2017-01-05 19:20:47 -0600

    Update build_bitshares.sh

--- bitshares-ubuntu-vm/build_bitshares.sh
+++ bitshares-ubuntu-vm/build_bitshares.sh
@@ -20,49 +20,53 @@ time apt-get -y install ntp g++ git make cmake libbz2-dev libdb++-dev libdb-dev 
 # Build BitShares from source                                                                    #
 ##################################################################################################
 cd /usr/local
-time git clone https://github.com/bitshares/bitshares-2.git
-cd bitshares-2/
+time git clone https://github.com/cryptonomex/graphene.git
+mv graphene/ graphene-testnet/
+cd graphene-testnet/
+git branch testnet
+git remote set-url origin https://github.com/BitSharesEurope/graphene-testnet
+git push origin testnet
 time git submodule update --init --recursive
 time cmake -DCMAKE_BUILD_TYPE=Release .
 time make -j$NPROC
 
-cp /usr/local/bitshares-2/programs/witness_node/witness_node /usr/bin/bitshares_witness_node
-cp /usr/local/bitshares-2/programs/cli_wallet/cli_wallet /usr/bin/bitshares_cli_wallet
+cp /usr/local/bitshares-2/programs/witness_node/witness_node /usr/bin/testnet_witness_node
+cp /usr/local/bitshares-2/programs/cli_wallet/cli_wallet /usr/bin/testnet_cli_wallet
 
 ##################################################################################################
 # Configure bitshares service. Enable it to start on boot.                                       #
 ##################################################################################################
-cat >/lib/systemd/system/bitshares.service <<EOL
+cat >/lib/systemd/system/testnet.service <<EOL
 [Unit]
-Description=Job that runs bitshares daemon
+Description=Job that runs testnet daemon
 [Service]
 Type=simple
-Environment=statedir=/home/$USER_NAME/bitshares/witness_node
-ExecStartPre=/bin/mkdir -p /home/$USER_NAME/bitshares/witness_node
-ExecStart=/usr/bin/bitshares_witness_node --rpc-endpoint=127.0.0.1:8090 -d /home/$USER_NAME/bitshares/witness_node
+Environment=statedir=/home/$USER_NAME/testnet/witness_node
+ExecStartPre=/bin/mkdir -p /home/$USER_NAME/testnet/witness_node
+ExecStart=/usr/bin/testnet_witness_node --rpc-endpoint=127.0.0.1:8090 -d /home/$USER_NAME/testnet/witness_node
 [Install]
 WantedBy=multi-user.target
 EOL
 
 systemctl daemon-reload
-systemctl enable bitshares
+systemctl enable testnet
 
 ##################################################################################################
-# Start the BitShares service to allow it to create the default configuration file. Stop the     #
+# Start the testnet service to allow it to create the default configuration file. Stop the       #
 # service, modify the config.ini file, then restart the service with the new settings applied.   #
 ##################################################################################################
-service bitshares start
+service testnet start
 wait 10
-sed -i 's/level=debug/level=info/g' /home/$USER_NAME/bitshares/witness_node/config.ini
-service bitshares stop
+sed -i 's/level=debug/level=info/g' /home/$USER_NAME/testnet/witness_node/config.ini
+service testnet stop
 wait 10
-service bitshares start
+service testnet start
 
 ##################################################################################################
 # Connect to host via SSH, then start cli wallet:                                                #
-# $sudo /usr/bin/cli_wallet --wallet-file=/usr/local/bitshares-2/programs/cli-wallet/wallet.json #
+# $sudo /usr/bin/cli_wallet --wallet-file=/usr/local/testnet/programs/cli-wallet/wallet.json     #
 # >set_password use_a_secure_password_but_check_your_shoulder_as_it_will_be_displayed_on_screen  #
 # >ctrl-d [will save the wallet and exit the client]                                             #
-# $nano /usr/local/bitshares-2/programs/cli-wallet/wallet.json                                   #
+# $nano /usr/local/testnet/programs/cli-wallet/wallet.json                                       #
 # Learn more: http://docs.bitshares.eu                                                           #
 ##################################################################################################
