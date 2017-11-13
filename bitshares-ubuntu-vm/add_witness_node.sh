7e1249d264f562d8e7302b2b185202b5de9f5e97
Ryan R. Fox <ryan@ryanrfox.com>
2017-11-13 10:53:31 -0500

    Change: Ubuntu 17.10 and fc repo

--- bitshares-ubuntu-vm/add_witness_node.sh
+++ bitshares-ubuntu-vm/add_witness_node.sh
@@ -57,22 +57,17 @@ cd /usr/local/src
 time git clone $GITHUB_REPOSITORY
 cd $PROJECT
 time git checkout $BRANCH
+##################################################################################################
+# TEST THE NEW FC BUILD HERE                                                                     #
+##################################################################################################
+sed -i 's/bitshares/bitshares-fc/aautushka/bitshares-fc/g' /usr/local/src/$PROJECT/.gitmodule
 time git submodule update --init --recursive
 
-sed -i 's/add_subdirectory( tests )/#add_subdirectory( tests )/g' /usr/local/src/$PROJECT/CMakeLists.txt
-sed -i 's/add_subdirectory(tests)/#add_subdirectory(tests)/g' /usr/local/src/$PROJECT/libraries/fc/CMakeLists.txt
-sed -i 's%auto history_plug = node->register_plugin%//auto history_plug = node->register_plugin%g' /usr/local/src/$PROJECT/programs/witness_node/main.cpp
-sed -i 's%auto market_history_plug = node->register_plugin%//auto market_history_plug = node->register_plugin%g' /usr/local/src/$PROJECT/programs/witness_node/main.cpp
 sed -i 's%include_directories( vendor/equihash )%#include_directories( vendor/equihash )%g' /usr/local/src/$PROJECT/libraries/fc/CMakeLists.txt
 sed -i 's%src/crypto/equihash.cpp%#src/crypto/equihash.cpp%g' /usr/local/src/$PROJECT/libraries/fc/CMakeLists.txt
 sed -i 's%add_subdirectory( vendor/equihash )%#add_subdirectory( vendor/equihash )%g' /usr/local/src/$PROJECT/libraries/fc/CMakeLists.txt
 sed -i 's%${CMAKE_CURRENT_SOURCE_DIR}/vendor/equihash%#${CMAKE_CURRENT_SOURCE_DIR}/vendor/equihash%g' /usr/local/src/$PROJECT/libraries/fc/CMakeLists.txt
 sed -i 's%target_link_libraries( fc PUBLIC ${LINK_USR_LOCAL_LIB} equihash ${%target_link_libraries( fc PUBLIC ${LINK_USR_LOCAL_LIB} ${%g' /usr/local/src/$PROJECT/libraries/fc/CMakeLists.txt
-sed -i 's/add_subdirectory( debug_node )/#add_subdirectory( debug_node )/g' /usr/local/src/$PROJECT/programs/CMakeLists.txt
-sed -i 's/add_subdirectory( delayed_node )/#add_subdirectory( delayed_node )/g' /usr/local/src/$PROJECT/programs/CMakeLists.txt
-sed -i 's/add_subdirectory( genesis_util )/#add_subdirectory( genesis_util )/g' /usr/local/src/$PROJECT/programs/CMakeLists.txt
-sed -i 's/add_subdirectory( size_checker )/#add_subdirectory( size_checker )/g' /usr/local/src/$PROJECT/programs/CMakeLists.txt
-sed -i 's/add_subdirectory( js_operation_serializer )/#add_subdirectory( js_operation_serializer )/g' /usr/local/src/$PROJECT/programs/CMakeLists.txt
 
 time cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE .
 time make -j$NPROC
